9cbe0813fc530af176b96d4df8c7dbfbabc26555
MarcelloSA <m_sakalauskas@hotmail.com>
2022-11-02 16:56:28 -0300

    atualização do exercício

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,44 @@
-/* import { BrowserRouter, Route, Switch } from "react-router-dom";
-
-import Cabecalho from "./components/Cabecalho/Cabecalho";
-import Home from "./pages/Home/Home"; */
-
-import ListaPosts from "./ListaPosts";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [itens, setItens] = useState([]);
+  useEffect(() => {
+    async function getItens() {
+      try {
+        const res = await fetch("https://fakestoreapi.com/products");
+        const dados = await res.json();
+        setItens(dados);
+      } catch (error) {
+        console.log("Error: " + error.message);
+      }
+    }
+
+    getItens();
+  }, []);
+  console.log(itens);
+
   return (
     <>
-      <h2>Exercício 2</h2>
-      <div>
-        <ListaPosts />
-      </div>
+      <h1>Exercício 2 </h1>
+      <section>
+        <ul>
+          {itens.map(({ id, title, image, price }) => (
+            <li key={id}>
+              <p>
+                <img class="foto-produto" src={image} alt="" />
+              </p>
+              <h2>{title}</h2>
+              <p>
+                {price.toLocaleString("pt-br", {
+                  style: "currency",
+                  currency: "BRL",
+                })}
+              </p>
+            </li>
+          ))}
+        </ul>
+      </section>
     </>
   );
 }
