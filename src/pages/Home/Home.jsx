247d8b7fe626498e05b6d593abe89cadc96b1e5f
MarcelloSA <103936996+msakal@users.noreply.github.com>
2022-11-03 10:29:48 -0300

    atualizando

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ListaPosts from "../../components/ListaPosts/ListaPosts";
+
+function Home() {
+  return (
+    <section>
+      <h2>Produtos</h2>
+      <ListaPosts />
+    </section>
+  );
+}
+
+export default Home;
